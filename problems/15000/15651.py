15593e620f6702000d5b9ff5cf964a03c53466fe
Juno <sojae0306@gmail.com>
2025-08-31 23:52:00 +0900

    8월 31일

---
+++ problems/15000/15651.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+
+seq = []
+
+
+def dfs(start):
+    if len(seq) == M:
+        print(" ".join(map(str, seq)))
+        return
+
+    for i in range(1, N + 1):
+        seq.append(i)
+        dfs(i)
+        seq.pop()
+
+
+dfs(1)
